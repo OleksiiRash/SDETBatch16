f575ed4e7d24df9566cc06220f4fa718ed46f346
OleksiiRash <alexey.rashevskyi@gmail.com>
2023-03-20 02:38:09 -0400

    homework_class2

--- src/class2/homework_class2/Variables.java
+++ src/class2/homework_class2/Variables.java
@@ -15,19 +15,16 @@ public class Variables {
          */
 
         String myName = "Alex";
-        String lasName = "Rash";
+        String lastName = "Rash";
         char myGrade = 'B';
-        myGrade = 'A'; // Task 3: updated data
         int myAge = 33;
         String myCity = "Madrid";
-        myCity = "Valencia"; // Task 3: updated data
         String myCountry = "Spain";
         String myPhoneNumber = "+34457845124";
-        myPhoneNumber = "+34457845245"; // Task 3: updated data
         boolean myCitizenshipStatus = false;
         String divider = "-------------------------------------------------------";
 
-        System.out.println("My name is" + " " + myName + " " + "and my last name is" + " " + lasName);
+        System.out.println("My name is" + " " + myName + " " + "and my last name is" + " " + lastName);
         System.out.println("I am" + " " + myGrade + " " + "student");
         System.out.println("My age is" + " " + myAge + " " + "years old");
         System.out.println("I live in" + " " + myCity + " " + "and country" + " " + myCountry);
@@ -38,8 +35,13 @@ public class Variables {
         if (myCitizenshipStatus == false) {
             System.out.println("And my citizenship status: Temporary Residence");}
 
-        // Task 3: updated data
+        // Task 3: Change student’s city, state, phone number and grade. And print those updated values
+
+        myGrade = 'A'; // Task 3: updated data
+        myCity = "Valencia"; // Task 3: updated data
+        myPhoneNumber = "+34457845245"; // Task 3: updated data
+
         System.out.println(divider);
-        System.out.println("My name is" + " " + myName + " " + "and I moved to new city" + " " + myCity + " " + "my new phone number is:" + " " + myPhoneNumber);
+        System.out.println("My name is" + " " + myName + " " + "and I moved to new city" + " " + myCity + " " + "and my new phone number is:" + " " + myPhoneNumber);
     }
 }
