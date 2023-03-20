b525d6a4bec15973ee81a63ac61ee9f61d20880d
OleksiiRash <alexey.rashevskyi@gmail.com>
2023-03-20 01:54:10 -0400

    homework_class2

---
+++ src/class2/homework_class2/VariableValues.java
@@ -0,0 +1,38 @@
+package class2.homework_class2;
+
+public class VariableValues {
+    public static void main(String[] args) {
+        /*
+        Task 1: Create a Java program and name it Variable Values
+        In your program declare variables using different ways to store all different primitive data.
+        Update value of all variables
+        Print the value of each updated variable
+         */
+
+        byte myAge = 33;
+        myAge = 34;
+        short myHeight = 174;
+        myHeight = 176;
+        int myWeight = 83;
+        myWeight = 85;
+        long myLifeExpectancy = 900000000L;
+        myLifeExpectancy = 990000000L;
+        float myBMI = 23.5f;
+        myBMI = 24.5f;
+        double myBankBalance = 75095.55;
+        myBankBalance = 85095.55;
+        boolean myCitizenshipStatus = false;
+        myCitizenshipStatus = true;
+        char myFirstInitial = 'R';
+        myFirstInitial = 'O';
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myWeight);
+        System.out.println(myLifeExpectancy);
+        System.out.println(myBMI);
+        System.out.println(myBankBalance);
+        System.out.println(myCitizenshipStatus);
+        System.out.println(myFirstInitial);
+
+    }
+}
---
+++ src/class2/homework_class2/Variables.java
@@ -0,0 +1,45 @@
+package class2.homework_class2;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        /*
+        Task 2: Create a Java program and name it Variables
+        In your program create different type of variables to store student’s information:
+        (name, last name, grade, city, state, phone number)
+        and then print value of those variables in the format:
+        My name is __ and my last name is __
+        I am __ student
+        I live in city and country __
+        And my phone number is __
+         */
+
+        String myName = "Alex";
+        String lasName = "Rash";
+        char myGrade = 'B';
+        myGrade = 'A'; // Task 3: updated data
+        int myAge = 33;
+        String myCity = "Madrid";
+        myCity = "Valencia"; // Task 3: updated data
+        String myCountry = "Spain";
+        String myPhoneNumber = "+34457845124";
+        myPhoneNumber = "+34457845245"; // Task 3: updated data
+        boolean myCitizenshipStatus = false;
+        String divider = "-------------------------------------------------------";
+
+        System.out.println("My name is" + " " + myName + " " + "and my last name is" + " " + lasName);
+        System.out.println("I am" + " " + myGrade + " " + "student");
+        System.out.println("My age is" + " " + myAge + " " + "years old");
+        System.out.println("I live in" + " " + myCity + " " + "and country" + " " + myCountry);
+        System.out.println("My phone number is" + " " + myPhoneNumber);
+
+        if (myCitizenshipStatus == true) {
+            System.out.println("And my citizenship status: Spanish nationality not by origin");}
+        if (myCitizenshipStatus == false) {
+            System.out.println("And my citizenship status: Temporary Residence");}
+
+        // Task 3: updated data
+        System.out.println(divider);
+        System.out.println("My name is" + " " + myName + " " + "and I moved to new city" + " " + myCity + " " + "my new phone number is:" + " " + myPhoneNumber);
+    }
+}
