435a25dbb9394d1c0fef064b787719a7ae82555d
OleksiiRash <alexey.rashevskyi@gmail.com>
2023-04-09 03:24:57 +0200

    class9

--- src/class9/homework_class9/Task8.java
+++ src/class9/homework_class9/Task8.java
@@ -6,7 +6,7 @@ public class Task8 {
         // Task 8 Create an array to store char values and then print those in reverse order
 
         char[] grade = {'A', 'B', 'C', 'D'};
-        for (int i = grade.length - 1; i >= 0; i--) {
+        for (int i = grade.length - 1; i >= 0; i--) { // grade.length-1 = 4-1
             System.out.print(grade[i] + " ");
         }
 
